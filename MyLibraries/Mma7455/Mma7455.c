fa4d490a9c45de3ac1f7ba8fda146a8c8c31aa80
Michal Ksiezopolski <mx>
2017-03-12 22:17:56 +0100

    Created MMA7455 project

---
+++ MyLibraries/Mma7455/Mma7455.c
@@ -0,0 +1,8 @@
+/*
+ * Mma7455.c
+ *
+ *  Created on: 12.03.2017
+ *      Author: mik
+ */
+
+
---
+++ MyLibraries/Mma7455/mma7455.h
@@ -0,0 +1,13 @@
+/*
+ * mma7455.h
+ *
+ *  Created on: 12.03.2017
+ *      Author: mik
+ */
+
+#ifndef MYLIBRARIES_MMA7455_MMA7455_H_
+#define MYLIBRARIES_MMA7455_MMA7455_H_
+
+
+
+#endif /* MYLIBRARIES_MMA7455_MMA7455_H_ */
